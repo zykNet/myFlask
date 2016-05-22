393c4c0c07cd76de6520e0992d64f6827858df7a
zykNet <602836219@qq.com>
2016-05-22 23:33:36 +0800

    3

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from flask import Flask
 
 app = Flask(__name__)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return "This Python."
+	user = {'nickname': 'Magic'}
+	posts =[{'author': {'nickname': 'John'}, 
+            'body': '1Beautiful day in Portland!' },
+			{'author': {'nickname': 'John'}, 
+            'body': '1Beautiful day in Portland!' },
+			{'author': {'nickname': 'John'}, 
+            'body': '1Beautiful day in Portland!' },
+			{'author': {'nickname': 'John'}, 
+            'body': '1Beautiful day in Portland!' },
+			{'author': {'nickname': 'John'}, 
+            'body': '1Beautiful day in Portland!' },
+			{'author': {'nickname': '2John'}, 
+            'body': '2Beautiful day in Portland!' }]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
+
